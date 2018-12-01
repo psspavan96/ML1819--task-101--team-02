fedf0ab7fc9b4b6e16f400cc47eb3e3e1e56a58a
lambaa@tcd.ie <dj.akashds@gmail.com>
2018-12-01 14:20:52 +0000

    log

--- sklearn/KNN_CarInsurance.py
+++ sklearn/KNN_CarInsurance.py
@@ -115,9 +115,9 @@ from sklearn.metrics import mean_squared_error
 from math import sqrt
 
 rms = sqrt(mean_squared_error(y_test, y_pred))
-print(rms)
+print("RSME: ", rms)
 mse = mean_squared_error(y_test, y_pred)
-print(mse)
+print("MSE: ", mse)
 
 
 # In[ ]:
